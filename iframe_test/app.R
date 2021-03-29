5fc46a7aa372774da64c9e3d1f809a0147f3665d
Fang <15221009458@163.com>
2021-03-29 16:33:39 +0800

    test github action

--- iframe_test/app.R
+++ iframe_test/app.R
@@ -2,22 +2,39 @@ library(shiny)
 library(ggplot2)
 
 ui <- fluidPage(
-  numericInput("point_num", "输入点的数量", value = 100, min = 10, max = 500, step = 1),
-  plotOutput("scatter_plot")
+  fluidRow(
+    column(width = 2, numericInput("point_num", "输入点的数量", value = 100, step = 1)),
+    column(width = 2, br(), actionButton("plot", "REPLOT"))
+  ),
+  fluidRow(
+    column(width = 6, plotOutput("scatter_plot")),
+    column(width = 6, plotOutput("rect_plot"))
+  )
 )
 
 server <- function(input, output, session) {
   dat <- reactive({
-    data.frame(
-      long = rnorm(input$point_num),
-      lat = rnorm(input$point_num)
+    input$plot
+    isolate(
+      data.frame(
+        long = rnorm(input$point_num),
+        lat = rnorm(input$point_num)
+      )
     )
   })
   
   output$scatter_plot <- renderPlot({
-    ggplot(dat(), aes(x = long, y = lat)) +
+    input$plot
+    ggplot(isolate(dat()), aes(x = long, y = lat)) +
       geom_point()
   })
+  
+  output$rect_plot <- renderPlot({
+    input$plot
+    ggplot(isolate(dat()), aes(x = long, y = lat)) + 
+      geom_bin2d(bins = 10, drop = FALSE)
+  })
+  
 }
 
 shinyApp(ui, server)
