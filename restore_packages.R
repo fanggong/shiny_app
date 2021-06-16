eecb468f7d3fb3073071eb5e9531fa8496aa3721
Fang <15221009458@163.com>
2021-06-16 09:47:45 +0800

    deactivate github action

--- restore_packages.R
+++ restore_packages.R
@@ -1,12 +1,19 @@
 
+cat("Checking for the presence of renv...\n")
+
 if (!require(renv)) {
+  cat("Install package renv...\n")
   install.packages("renv", repos = "http://cran.us.r-project.org")
 }
 
 dirs <- dir()
 dirs <- dirs[file.info(dirs)$isdir]
 
+cat("Waiting for process...\n")
+
 for (each in dirs) {
+  cat("Start process", each, "\n")
   renv::restore(each)
   renv::isolate(each)
+  cat("Over process", each, "\n")
 }
