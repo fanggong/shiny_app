9e465a5a4ca7e18d1bc7396c19c3fe5cd5ac179a
fangyongchao <yongchao.fang@outlook.com>
2023-04-26 03:08:10 +0800

    flipdownWidgets statitics

---
+++ flipdownWidgets_stat/app.R
@@ -0,0 +1,64 @@
+library(cranlogs)
+library(shiny)
+library(shinyWidgets)
+library(data.table)
+library(lubridate)
+library(plotly)
+
+src <- cran_downloads("flipdownWidgets", from = "2020-01-01")
+setDT(src)
+src <- src[count != 0]
+src[
+  , `:=`(
+    week = floor_date(as.Date(date), unit = "week", week_start = 1),
+    month = floor_date(as.Date(date), unit = "month")
+  )
+]
+day <- src[, .(count = sum(count)), .(period = date)]
+week <- src[, .(count = sum(count)), .(period = week)]
+month <- src[, .(count = sum(count)), .(period = month)]
+
+ui <- fluidPage(
+  fluidRow(
+    column(
+      width = 6,
+      offset = 6,
+      radioGroupButtons(
+        "period", choices = c("Day" = "day", "Week" = "week", "Month" = "month"),
+        direction = "horizontal", justified = FALSE, size = "normal"
+      )
+    ),
+    column(
+      width = 12,
+      plotlyOutput("downloads_trend")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  period <- reactive({
+    input$period
+  })
+  
+  output$test <- renderText({
+    period()
+  })
+  
+  output$downloads_trend <- renderPlotly({
+    plot_ly(get(period()), type = "scatter", mode = "lines") %>%
+      add_trace(
+        x = ~period, y = ~count, name = period(),
+        line = list(color = "#0F2540", width = 2), opacity = 0.5
+      ) %>%
+      layout(
+        xaxis = list(title = "Date", rangeslider = list(visible = TRUE)),
+        yaxis = list(title = "Download Times"),
+        showlegend = FALSE,
+        template = 1121
+      )
+  })
+}
+
+shinyApp(ui, server)
+
+
